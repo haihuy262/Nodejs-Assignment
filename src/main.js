bd1fbfd8fde16b2853bc65775ffc8b2d2a1ebfa8
haihuy262 <dolcegabana0206@gmail.com>
2023-11-10 13:15:54 +0700

    add

--- src/main.js
+++ src/main.js
@@ -14,6 +14,18 @@ app.get("/register", (req, res) => {
   res.render("Register.ejs");
 });
 
+app.get("/addproducts", (req, res) => {
+  res.render("AddProducts.ejs");
+});
+
+app.get("/listproducts", (req, res) => {
+  res.render("ListProducts.ejs");
+});
+
+app.get("/editproducts", (req, res) => {
+  res.render("EditProducts.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
