5c3b6239ae781ac117eee2d42ec0b4a659dbcf1d
haihuy262 <dolcegabana0206@gmail.com>
2023-11-12 17:33:26 +0700

    delete

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const path = require("path");
 const user = require("./model/user");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const product = require("./model/product");
 
 const app = express();
 const port = 3000;
@@ -35,8 +36,14 @@ app.get("/addproducts", (req, res) => {
   res.render("AddProducts.ejs");
 });
 
-app.get("/listproducts", (req, res) => {
-  res.render("ListProducts.ejs");
+app.get("/listproducts", async (req, res) => {
+  try {
+    const products = await product.find();
+    res.render("ListProducts.ejs", { products });
+  } catch (error) {
+    console.error("Lỗi khi truy vấn sản phẩm:", error);
+    res.status(500).send("Lỗi khi truy vấn sản phẩm");
+  }
 });
 
 app.get("/homeproducts", (req, res) => {
@@ -62,16 +69,63 @@ app.post("/register/user", async (req, res) => {
     res.status(500).json({ error: "Đã xảy ra lỗi khi đăng ký người dùng." });
   }
 });
+
 app.post("/login", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
 
-  const users = await user.findOne({ email: email, password: password });
+  try {
+    // Find the user by email in the database
+    const existingUser = await user.findOne({ email });
 
-  if (users) {
-    res.send("Đăng nhập thành công");
-  } else {
-    res.status(400).json({ message: "Wrong account or password" });
+    // Check if the user exists and the password is correct
+    if (existingUser && existingUser.password === password) {
+      // Redirect to the new page upon successful login
+      res.redirect("/homeproducts"); // Replace "/dashboard" with the route you want to redirect to
+    } else {
+      res.status(401).json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error("Lỗi khi đăng nhập:", error);
+    res.status(500).json({ error: "Đã xảy ra lỗi khi đăng nhập." });
+  }
+});
+
+app.post("/addproducts/product", async (req, res) => {
+  const newProduct = new product({
+    maSanPham: req.body.maSanPham,
+    tenSanPham: req.body.tenSanPham,
+    giaSanPham: req.body.giaSanPham,
+    nhaSanXuat: req.body.nhaSanXuat,
+    anhMinhHoa: req.body.anhMinhHoa,
+    mauSac: req.body.mauSac,
+    loaiSanPham: req.body.loaiSanPham,
+    maKhachHang: req.body.maKhachHang,
+    tenKhachHang: req.body.tenKhachHang,
+  });
+  try {
+    const result = await newProduct.save();
+    res.redirect("/listproducts");
+  } catch (error) {
+    console.error("Lỗi khi thêm sản phẩm:", error);
+    res.status(500).json({ error: "Đã xảy ra lỗi khi thêm sản phẩm." });
+  }
+});
+
+app.delete("/deleteproduct/:productId", async (req, res) => {
+  const productId = req.params.productId;
+  try {
+    const result = await product.deleteOne({ _id: productId });
+
+    if (result.deletedCount === 1) {
+      res.json({ success: true, message: "Xoá sản phẩm thành công." });
+    } else {
+      res
+        .status(404)
+        .json({ success: false, message: "Không tìm thấy sản phẩm." });
+    }
+  } catch (error) {
+    console.error("Lỗi khi xoá sản phẩm:", error);
+    res.status(500).json({ success: false, message: "Lỗi khi xoá sản phẩm." });
   }
 });
 
---
+++ src/model/product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  maSanPham: String,
+  tenSanPham: String,
+  giaSanPham: String,
+  nhaSanXuat: String,
+  anhMinhHoa: String,
+  mauSac: String,
+  loaiSanPham: String,
+  maKhachHang: String,
+  tenKhachHang: String,
+});
+const product = mongoose.model("products", UserSchema);
+module.exports = product;
